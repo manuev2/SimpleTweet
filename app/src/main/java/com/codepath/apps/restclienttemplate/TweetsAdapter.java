6daf65e78370682e77638f3c3e75bd1f9e31452f
manuev2 <manny.4163@gmail.com>
2021-01-24 18:38:16 -0800

    Time since posted added

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -67,15 +67,18 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         ImageView ivProfileImage;
         TextView tvBody;
         TextView tvScreenName;
+        TextView tvTime;
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvBody = itemView.findViewById(R.id.tvBody);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvTime = itemView.findViewById(R.id.tvTime);
         }
 
         public void bind(Tweet tweet){
+            tvTime.setText(TimeFormatter.getTimeDifference(tweet.createdAt));
             tvBody.setText(tweet.body);
             tvScreenName.setText(tweet.user.screenName);
             Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
