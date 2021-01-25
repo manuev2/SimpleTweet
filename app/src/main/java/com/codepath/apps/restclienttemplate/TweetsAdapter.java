e43c4a2cae10b124db2c5f94812422ab7fbac449
manuev2 <manny.4163@gmail.com>
2021-01-24 19:34:42 -0800

    Reformatting item tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -68,6 +68,7 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         TextView tvBody;
         TextView tvScreenName;
         TextView tvTime;
+        TextView tvName;
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
@@ -75,12 +76,14 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
             tvBody = itemView.findViewById(R.id.tvBody);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
             tvTime = itemView.findViewById(R.id.tvTime);
+            tvName = itemView.findViewById(R.id.tvName);
         }
 
         public void bind(Tweet tweet){
             tvTime.setText(TimeFormatter.getTimeDifference(tweet.createdAt));
             tvBody.setText(tweet.body);
-            tvScreenName.setText("@" + tweet.user.screenName);
+            tvScreenName.setText("@" + tweet.user.screenName + " \u2022");
+            tvName.setText(tweet.user.name);
             Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
 
         }
